5c0858161dc6f1e846cc65658b0cd615e27054fe
Muhtasim Rabib <muhtasimrabib2@gmail.com>
2026-07-15 17:08:22 +0600

    adding the counter to grocery list so that items with the same names does not appear twice in the list

--- 02 grocery-List-App/src/App.jsx
+++ 02 grocery-List-App/src/App.jsx
@@ -1,26 +1,64 @@
 import "./App.css";
+import { useState } from "react";
 import groceryCartImage from "./assets/grocery-cart.png";
 
+// { name: "banana", quantity: 1, completedStatus: true },
+
 function App() {
+  const [inuptValue, setInputvalue] = useState("");
+  const [groceryItems, SetGroceryItems] = useState([]);
+
+  const handleChangeInputvalue = function (e) {
+    setInputvalue(e.target.value);
+  };
+
+  const handleAddGroceryItems = function (e) {
+    if (e.key === "Enter") {
+      if (inuptValue) {
+        SetGroceryItems([
+          ...groceryItems,
+          { name: inuptValue, quantity: 1, completedStatus: false },
+        ]);
+        setInputvalue(" ");
+      }
+    }
+  };
+
+  const renderGroceryListItemMarkup = () => {
+    return groceryItems.map((item) => (
+      <li key={item.name}>
+        <div className="container">
+          <div className="container-part-1">
+            <input type="checkbox" />
+            <p>{item.name}</p>
+          </div>
+          <div className="container-part-2">
+            <button className="remove-button">X</button>
+          </div>
+        </div>
+      </li>
+    ));
+  };
+
   return (
     <main className="App">
       <div className="">
         <h4 className="success">You're Done</h4>
         <div className="header">
           <h1>Shopping List</h1>
+          {/* {JSON.stringify(groceryItems)} To understand */}
           <img src={groceryCartImage} alt="" />
-          <input type="text" placeholder="Add an item" />
+          <input
+            type="text"
+            placeholder="Add an item"
+            className="item-input"
+            onChange={handleChangeInputvalue}
+            value={inuptValue}
+            onKeyDown={handleAddGroceryItems}
+          />
         </div>
         <div>
-          <ul>
-            <li>
-              <div className="container">
-                <input type="checkbox" />
-                <p>Carrots</p>
-                <button className="remove-button">x</button>
-              </div>
-            </li>
-          </ul>
+          <ul>{renderGroceryListItemMarkup()}</ul>
         </div>
       </div>
     </main>
