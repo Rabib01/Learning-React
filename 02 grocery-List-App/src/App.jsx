0d58acf777b78b52acbfaa1a679e3f02872a260c
Muhtasim Rabib <muhtasimrabib2@gmail.com>
2026-07-17 20:49:30 +0600

    still working on this project

--- 02 grocery-List-App/src/App.jsx
+++ 02 grocery-List-App/src/App.jsx
@@ -4,24 +4,48 @@ import groceryCartImage from "./assets/grocery-cart.png";
 
 // { name: "banana", quantity: 1, completedStatus: true },
 
+// t.b.c 2:13:17 from the findIndex() method ||https://www.youtube.com/watch?v=BWJ01f-s3S4
+
 function App() {
   const [inuptValue, setInputvalue] = useState("");
   const [groceryItems, SetGroceryItems] = useState([]);
 
   const handleChangeInputvalue = function (e) {
+    // console.log(e.target.value);
     setInputvalue(e.target.value);
   };
 
   const handleAddGroceryItems = function (e) {
     if (e.key === "Enter") {
       if (inuptValue) {
-        SetGroceryItems([
-          ...groceryItems,
-          { name: inuptValue, quantity: 1, completedStatus: false },
-        ]);
-        setInputvalue(" ");
+        // console.log(groceryItems);
+        const updatedGroceryList = [...groceryItems];
+        const indexItem = updatedGroceryList.findIndex(
+          (item) => item.name === inuptValue,
+        );
+        // console.log(indexItem);
+        if (indexItem !== -1) {
+          updatedGroceryList[indexItem].quantity++;
+        } else {
+          // console.log(updatedGroceryList);
+          updatedGroceryList.push({
+            name: inuptValue,
+            quantity: 1,
+            completedStatus: false,
+          });
+        }
+        SetGroceryItems(updatedGroceryList);
+        setInputvalue("");
       }
     }
+  };
+
+  const handleRemoveItemsFromList = function (name) {
+    // console.log(updatedGroceryList, name);
+
+    // SetGroceryItems([...groceryItems].filter((item) => item.name !== name));
+    // dont need to spread as filter already creates a new array. groceryItems ---> array A ----> spread array B -----> filter array C
+    SetGroceryItems(groceryItems.filter((item) => item.name !== name));
   };
 
   const renderGroceryListItemMarkup = () => {
@@ -30,10 +54,22 @@ function App() {
         <div className="container">
           <div className="container-part-1">
             <input type="checkbox" />
-            <p>{item.name}</p>
+            <p>
+              {item.name}{" "}
+              {/* {item.quantity > 1 ? <span>x{item.quantity}</span> : null} */}
+              {/* I think I am using nullish coalescent operator to short circuit this ?? */}
+              {item.quantity > 1 && <span>x{item.quantity}</span>}
+            </p>
           </div>
           <div className="container-part-2">
-            <button className="remove-button">X</button>
+            <button
+              className="remove-button"
+              onClick={() => {
+                handleRemoveItemsFromList(item.name);
+              }}
+            >
+              X
+            </button>
           </div>
         </div>
       </li>
